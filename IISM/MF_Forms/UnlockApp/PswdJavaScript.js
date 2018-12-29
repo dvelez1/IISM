1439e7fe60da8197290de01c1ca87df9d97f976b
Dennis Velez <velez.dennis0@outlook.com>
2018-12-29 17:37:21 -0400

    Work in progress!

---
+++ IISM/MF_Forms/UnlockApp/PswdJavaScript.js
@@ -0,0 +1,23 @@
+﻿
+var button = document.getElementById("submit");
+button.onclick = MyFunction;
+
+function MyFunction() {
+
+
+    var value1 = document.getElementById("psw1").value;
+    var value2 = "unlock-KE";
+
+    if (value1 == value2)
+    {
+        alert('EQUAL');
+        return true;
+    }
+    else
+    {
+        alert('NO EQUAL');
+        return false;
+    }
+
+
+}
